7ad64a2ee63b90cf11c9454897755306889b792b
Andres179012 <andresmercado389@gmail.com>
2021-12-11 17:48:40 -0600

    Reportes ************60%

---
+++ CapaModelo/ReporteCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo
+{
+    public class ReporteCompra
+    {
+        public string FechaRegistro { get; set; }
+        public Usuario oUsuario { get; set; }
+        public int IdUsuario { get; set; }
+        public string TotalCosto { get; set; }
+        public string Nombres { get; set; }
+        public string NombreComercial { get; set; }
+        public string Concentracion { get; set; }
+    }
+}
--- FarmaciaSaori/Controllers/ReporteController.cs
+++ FarmaciaSaori/Controllers/ReporteController.cs
@@ -23,6 +23,11 @@ namespace FarmaciaSaori.Controllers
             return View();
         }
 
+        public ActionResult Compras()
+        {
+            return View();
+        }
+
         public JsonResult ObtenerProducto(int idtienda, string codigoproducto)
         {
             List<ReporteProducto> lista = CD_Reportes.Instancia.ReporteProductoTienda(idtienda, codigoproducto);
@@ -35,6 +40,13 @@ namespace FarmaciaSaori.Controllers
         {
             
             List<ReporteVenta> lista = CD_Reportes.Instancia.ReporteVenta(Convert.ToDateTime(fechainicio), Convert.ToDateTime(fechafin), idtienda);
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObtenerCompra(string fechainicio, string fechafin, int idusuario)
+        {
+
+            List<ReporteCompra> lista = CD_Reportes.Instancia.ReporteCompra(Convert.ToDateTime(fechainicio), Convert.ToDateTime(fechafin), idusuario);
             return Json(lista, JsonRequestBehavior.AllowGet);
         }
 
